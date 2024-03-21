7782f497c24aff516926f5f5f5902dd1e5a70a12
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-21 21:02:39 +0200

    db test.

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- script that ranks country origins of bands
-SELECT origin, SUM(fans) as bands
+SELECT origin, SUM(fans) as band
 FROM metal_bands
 GROUP BY origin
-ORDER BY bands DESC;
+ORDER BY band DESC;
