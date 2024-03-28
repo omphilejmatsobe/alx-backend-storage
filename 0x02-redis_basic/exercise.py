a9e3ef343b41bbb01a5ffd070600b9bf348be9dc
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-28 02:20:22 +0200

    Callable increment method implemented

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,8 @@ Using Redis NoSQL database
 import redis
 import uuid
 from typing import Any, Callable, Union
+from functools import wraps
+
 
 def count_calls(method: Callable) -> Callable:
     """
@@ -35,6 +37,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         takes a data argument and returns a string
